9f742df0b23166ad2b39d39bb990a378dd47df42
Andrii Soluk <isoluchok@gmail.com>
2023-07-21 07:58:06 +0300

    first commit

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestSum(t *testing.T) {
+	// just a comment
+	time.Sleep(time.Minute)
+	result := sum(1, 3)
+	if result != 4 {
+		t.Error("got wrong result value")
+	}
+}
