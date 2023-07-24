045133e0f0afc0d87c3d117264c15bf06d9b8996
Andrii Soluk <isoluchok@gmail.com>
2023-07-24 08:59:34 +0300

    hour
    commit-id:9783e012

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestSum(t *testing.T) {
 	// just a comment
-	time.Sleep(time.Minute)
+	time.Sleep(time.Hour)
 	result := sum(1, 3)
 	if result != 4 {
 		t.Error("got wrong result value!!")
