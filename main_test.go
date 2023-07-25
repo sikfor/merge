e2a50e79c76df2e96ffecb449c7191a51e4e0475
Andrii Soluk <isoluchok@gmail.com>
2023-07-25 10:15:54 +0300

    del two comments

--- main_test.go
+++ main_test.go
@@ -7,10 +7,9 @@ import (
 
 func TestSum(t *testing.T) {
 	// just a comment
-	// comment one
-	// comment two
 	// comment three
 	time.Sleep(time.Minute * 5)
+	time.Sleep(time.Minute * 4)
 	result := sum(1, 3)
 	if result != 4 {
 		t.Error("got wrong result value")
